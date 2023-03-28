cdc41d8f271fc0a11497bfe24172ff72a7d9705e
GeorgeDong32 <georgedong32@foxmail.com>
2023-03-28 22:44:57 +0800

    Update

--- Self Learn/HDLBits-Module/Module_shift8.v
+++ Self Learn/HDLBits-Module/Module_shift8.v
@@ -4,7 +4,7 @@ module top_module (
     input [1:0] sel, 
     output [7:0] q 
 );
-    wire q1, q2, q3;
+    reg [7:0] q1, q2, q3;
     my_dff8 dff1(clk, d, q1);
     my_dff8 dff2(clk, q1, q2);
     my_dff8 dff3(clk, q2, q3);
