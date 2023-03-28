8c1271240a10e51d1d9dbd48c80d2f1574ba6e58
George·Dong <georgedong32@foxmail.com>
2023-03-28 23:43:27 +0800

    Update

--- Self Learn/HDLBits-Module/Module_fadd.v
+++ Self Learn/HDLBits-Module/Module_fadd.v
@@ -1,8 +1,9 @@
-module top_module(
+module top_module (
     input [31:0] a,
     input [31:0] b,
     output [31:0] sum
-);
+);//
+
     wire [15:0] sum_lo, sum_hi;
     wire jinwei;
     wire [15:0] a_lo, a_hi, b_lo, b_hi;
@@ -15,5 +16,12 @@ module top_module(
     add16 adder2( .a(a_hi), .b(b_hi), .sum(sum_hi), .cin(jinwei), .cout());
 
     assign sum = {sum_hi, sum_lo};
+
+endmodule
+
+module add1 ( input a, input b, input cin,   output sum, output cout );
+
+    assign sum = a ^ b ^ cin; // 异或运算
+    assign cout = (a & b) | (a & cin) | (b & cin); // 与或运算
 
 endmodule
